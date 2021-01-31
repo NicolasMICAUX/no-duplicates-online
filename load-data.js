ce86f29eec2fc665a94923552ed3ab5e7185a1d5
nmicaux <nicolas.micaux@telecom-paris.fr>
2021-01-31 23:49:24 +0100

    find similarity, compute embeddings,

--- load-data.js
+++ load-data.js
@@ -25,19 +25,37 @@ function load_from_textarea() {
 
 var count = 0;
 
-function add_item(item) {   // item of type text
-    var li = $("<li id='id'" + count.toString() + "><div>"+item+"</div></li>");
+function add_item(item, embedding) {   // item of type text
+    const li = $("<li id='id" + count.toString() + "'><div>" + item + "</div></li>");
     li.data('insideLevels', 0);
     li.data('upperLevels', 0);
     li.data('module', 'base');
+    li.data('value', item);
+    li.data('embedding', embedding);    // store precomputed embedding
     $('#sTree2').append( li );
     count++;
 }
 
 function add_items(lines) {   // list of Strings
-    for(let line = 0; line < lines.length; line++){
-        if (lines[line]) {
-            add_item(lines[line]);
+    // compute embeddings for new lines
+    tf_model.embed(lines).then(embeddings => {
+        // convert Tensor to Arrays
+        lines_embeddings = embeddings.arraySync();
+        //embeddings.print(true /* verbose */);
+
+        const current_elems = $('#sTree2').sortableListsToArray();
+
+        for (let line = 0; line < lines.length; line++) {
+            if (lines[line]) {
+                let sim = 0;
+                for (let i = 0; i < current_elems.length; i++) {
+                    sim = similarity(lines_embeddings[line], $('#'+current_elems[i].id).data('embedding'));
+                    // TODO : check SIMILARITY HIGH ENOUGH
+                }
+                //embeddings.dispose(); // supprimer un tensor de la mémoire
+
+                add_item(lines[line], lines_embeddings[line]);
+            }
         }
-    }
+    });
 }
