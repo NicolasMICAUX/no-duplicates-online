9e62863344cebfa19c7b88faefedbc9fbfe3c2b4
nmicaux <nicolas.micaux@telecom-paris.fr>
2021-01-31 21:33:57 +0100

    load data into draggable list

--- load-data.js
+++ load-data.js
@@ -2,9 +2,7 @@ function load_data_from_file(file) {
     const reader = new FileReader();
     reader.onload = function(progressEvent){
         const lines = this.result.split('\n');
-        for(let line = 0; line < lines.length; line++){
-            console.log(lines[line]);
-        }
+        add_items(lines);
     };
     reader.readAsText(file);
 }
@@ -21,8 +19,25 @@ function load_from_textarea() {
     const lines = area.value.replace(/\r\n/g,"\n").split("\n").filter(line => line);
     // https://stackoverflow.com/questions/2299604/javascript-convert-textarea-into-an-array
 
+    add_items(lines);
+    area.value = '';    // reset textarea
+}
+
+var count = 0;
+
+function add_item(item) {   // item of type text
+    var li = $("<li id='id'" + count.toString() + "><div>"+item+"</div></li>");
+    li.data('insideLevels', 0);
+    li.data('upperLevels', 0);
+    li.data('module', 'base');
+    $('#sTree2').append( li );
+    count++;
+}
+
+function add_items(lines) {   // list of Strings
     for(let line = 0; line < lines.length; line++){
-        console.log(lines[line]);
+        if (lines[line]) {
+            add_item(lines[line]);
+        }
     }
-    area.value = '';
 }
