97cfccd5e88ef2f2af9f90ec91eab27a29c3babc
nmicaux <nicolas.micaux@telecom-paris.fr>
2021-02-01 19:56:43 +0100

    export button and mechanism

--- load-data.js
+++ load-data.js
@@ -1,3 +1,4 @@
+
 // * load data from multplie files *
 // called by "input files"
 function load_data_from_files(files) {
@@ -45,6 +46,10 @@ function add_item(item, embedding) {   // item is a String
     count++;
 }
 
+// Array to keep distribution*
+var DISTRIB = [];
+var THRESHOLD = 0.8;
+
 // * add items, checking if not too similar to an existing item *
 function add_items(lines) {   // list of Strings
     // compute embeddings for new lines
@@ -62,16 +67,22 @@ function add_items(lines) {   // list of Strings
                 // similarity to all existing elements
                 let sim = [];
                 for (let i = 0; i < current_elems.length; i++) {
+                    // TODO : prevent same (to prevent aberration in distribution !)
                     // compute similarity
                     let simil = similarity(lines_embeddings[line], $('#'+current_elems[i].id).data('embedding'));
                     sim.push(simil);
+
+                    // add similarity to sim. distribution
+                    DISTRIB.pushSorted(simil);
+                    // TODO : update mean, variance..., so threshold
+
                     // TODO : remove in production
                     console.log(`${lines[line]}, ${$('#' + current_elems[i].id).data('value')}=${simil.toString()}`);
                 }
                 // check that new element is not too similar to all existing elements
                 let max = Math.max(...sim);
                 let idx_max = sim.indexOf(max);
-                if (max < 0.8) {
+                if (max < THRESHOLD) {
                     add_item(lines[line], lines_embeddings[line]);
                 }
                 else {
@@ -84,3 +95,56 @@ function add_items(lines) {   // list of Strings
         }
     });
 }
+
+// * download without duplicates *
+function download_no_duplicates() {
+    let elems = [];
+    const current_elems = $('#sTree2').sortableListsToHierarchy();
+    // push only top-level elements
+    for (let i = 0; i < current_elems.length; i++) {
+        elems.push(current_elems[i].value);
+    }
+    // download as text file
+    download('export.txt', elems.join("\n"));
+}
+
+// * download as file *
+// https://stackoverflow.com/questions/3665115/how-to-create-a-file-in-memory-for-user-to-download-but-not-through-server
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+// * UTILS *
+
+// * push an item into a sorted array *
+//https://stackoverflow.com/questions/42946561/how-can-i-push-an-element-into-array-at-a-sorted-index-position
+const sortByPrice = (a, b) => a > b;
+
+Array.prototype.pushSorted = function(el, compareFn=sortByPrice) {
+    this.splice((function(arr) {
+        let m = 0;
+        let n = arr.length - 1;
+
+        while(m <= n) {
+            let k = (n + m) >> 1;
+            let cmp = compareFn(el, arr[k]);
+
+            if(cmp > 0) m = k + 1;
+            else if(cmp < 0) n = k - 1;
+            else return k;
+        }
+
+        return -m - 1;
+    })(this), 0, el);
+
+    return this.length;
+};
